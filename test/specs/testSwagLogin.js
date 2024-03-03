7f52c43247e0b292babe699eed6783b9ff47eeba
Khandappa P <khandappamaratur@gmail.com>
2024-03-03 13:10:20 +1100

    added new test file for Sauce demo login test

---
+++ test/specs/testSwagLogin.js
@@ -0,0 +1,11 @@
+const { click } = require("webdriverio/build/commands/element")
+
+describe('The Swag demo login test' , ()=> {
+    it('The login test' , ()=>{
+        browser.url('https://www.saucedemo.com/v1/')
+        $('[//input[@id="user-name"]]').setValue("standard_user")
+        $('[//input[@id="password"]]').setValue("secret_user")
+        $('[//input[@class="btn_action"]]').click    
+
+    })
+})
