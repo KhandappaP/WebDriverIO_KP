14ffbdc4578bfae8adbdcfb0b19c0c768788a973
Khandappa P <khandappamaratur@gmail.com>
2024-03-03 13:15:38 +1100

    update on one element

--- test/specs/testSwagLogin.js
+++ test/specs/testSwagLogin.js
@@ -3,7 +3,7 @@ const { click } = require("webdriverio/build/commands/element")
 describe('The Swag demo login test' , ()=> {
     it('The login test' , ()=>{
         browser.url('https://www.saucedemo.com/v1/')
-        $('[//input[@id="user-name"]]').setValue("standard_user")
+        $('[//input[@id="user-name"]]').setValue("standard_userstan")
         $('[//input[@id="password"]]').setValue("secret_user")
         $('[//input[@class="btn_action"]]').click    
 
